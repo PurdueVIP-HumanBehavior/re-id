ac14d1a80b23c88dc6f5a28b26a54c84999c860d
Taher Dohadwala <taher.dohadwala1@gmail.com>
2019-10-09 10:14:53 -0400

    Added ResNet50 attribute support

--- vectorgenerator.py
+++ vectorgenerator.py
@@ -4,6 +4,9 @@ from MGN import MGN
 import torch
 from torch.autograd import Variable
 from torchvision import transforms
+from torch import nn
+from torch.nn import init
+from torchvision import models
 
 num_classes = 751  # change this depend on your dataset
 
@@ -28,7 +31,29 @@ class MGN_Wrap:
         return person
 
 
+class ResNet50_nFC_Wrap:
+	def __init__(self,class_num,weights_path):
+		self.model = ResNet50_FC(class_num):
+		self.load_state_dict(torch.load(weights_path))
+		self.model.cuda()
+		self.model.eval()
+		self.transform = transforms.Compose([
+            transforms.Resize((288, 144)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+    def getVect(self, person):
+    	person = self.transform(person).float()
+    	person = person.unsqueeze(dim=0)
+        person = Variable(person, requires_grad=True)
+        person = person.cuda()  #assumes that you're using GPU
+        person = self.model(person)
+        return person
+
+
 options = {
     opt.defaultkey: MGN_Wrap,
-    "MGN": MGN_Wrap
+    "MGN": MGN_Wrap,
+    'ResNet50_FC':ResNet50_nFC_Wrap
 }
