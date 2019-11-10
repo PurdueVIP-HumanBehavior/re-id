dbbfe9157f19b7422b34077ca4dbf3093ae3906f
ninja-grass <moizr95@gmail.com>
2019-11-10 10:21:23 -0500

    split video

---
+++ videoToFrames.py
@@ -0,0 +1,21 @@
+import cv2
+
+name = 'mov3'
+cap = cv2.VideoCapture(name + '.MOV')
+
+if cap.isOpened() == False:
+    print("error openning file")
+
+i = 0
+prefix = name + '/'
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+        namenum = '%05d'%i
+        cv2.imwrite(prefix + namenum + '.jpg', frame)
+        if i % 100 == 0:
+            print(i)
+        i = i + 1
+
+cap.release()
