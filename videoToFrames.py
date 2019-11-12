29684e0e733ebc53a113fe9bceb3fbe6bb348960
ninja-grass <moizr95@gmail.com>
2019-11-12 11:59:21 -0500

    creates folder too

--- videoToFrames.py
+++ videoToFrames.py
@@ -1,19 +1,23 @@
 import cv2
+import os
 
-name = 'mov3'
-cap = cv2.VideoCapture(name + '.MOV')
+path = '../reid-data'
+name = 'msee_NEside_Moiz'
+ext = '.mp4'
+cap = cv2.VideoCapture(os.path.join(path, name + ext))
+if name not in os.listdir(path):
+    os.makedirs(os.path.join(path, name))
 
 if cap.isOpened() == False:
     print("error openning file")
 
 i = 0
-prefix = name + '/'
 
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
         namenum = '%05d'%i
-        cv2.imwrite(prefix + namenum + '.jpg', frame)
+        cv2.imwrite(os.path.join(path, name, namenum + '.jpg'), frame)
         if i % 100 == 0:
             print(i)
         i = i + 1
