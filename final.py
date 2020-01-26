ce1834b314dc89a89994bd773832ba95f14103e2
Noureldin Hendy <nhendy@purdue.edu>
2020-01-26 14:57:38 -0500

    clean up sort.py (#2)
    * clean up sort.py

--- final.py
+++ final.py
@@ -9,7 +9,7 @@ from cropper import crop_image
 import os
 import numpy as np
 import cv2
-from mySort import Sort
+from sort import Sort
 import bboxtrigger
 from scipy.stats import mode
 from tqdm import tqdm
@@ -21,15 +21,17 @@ detector = detectors.options[args.detector]()
 vecgen = vectorgenerator.options[args.vectgen]()
 dataloader = loaders.getLoader(datapath, args.loader, args.interval)
 
+
 def getvec2out(path, vecgen):
     if os.path.isfile(path):
         moiz = Image.open(path)
         return vecgen.getVect2(moiz)
     return None
 
+
 def getMaxIndex(array, k=1):
     maxnums = array[0:k]
-    maxinds = np.arange(0,k)
+    maxinds = np.arange(0, k)
     minins = min(maxnums)
     mininin = maxnums.index(minins)
     for i, val in enumerate(array):
@@ -41,7 +43,8 @@ def getMaxIndex(array, k=1):
     if len(maxnums) == 1:
         return maxinds[0]
     else:
-        return [x for _,x in sorted(zip(maxnums, maxinds), reverse=True)]
+        return [x for _, x in sorted(zip(maxnums, maxinds), reverse=True)]
+
 
 def loadPredefGal(path):
     if not os.path.exists(path):
@@ -49,23 +52,29 @@ def loadPredefGal(path):
     imgfile = os.listdir(path)
     retval = list()
     for name in imgfile:
-        na = '.'.join(name.split('.')[:-1])
+        na = ".".join(name.split(".")[:-1])
         img = getvec2out(os.path.join(path, name), vecgen)
         retval.append(img)
     return retval
+
 
 ###############################################################
 def getVect(croppedimg):
     return vecgen.getVect2(croppedimg)
 
+
 # setup triggers
 refimg = cv2.imread(firstimgpath)
 chkcoord1 = [[1471, 67], [1487, 117]]
 sampcoord1 = [[1348, 72], [1640, 671]]
-trig1 = bboxtrigger.BboxTrigger('NE_Moiz', refimg, .27, .87, chkcoord1, sampcoord1, detector)
+trig1 = bboxtrigger.BboxTrigger(
+    "NE_Moiz", refimg, 0.27, 0.87, chkcoord1, sampcoord1, detector
+)
 chkcoord2 = [[354, 70], [375, 110]]
 sampcoord2 = [[114, 64], [600, 722]]
-trig2 = bboxtrigger.BboxTrigger('NE_Moiz', refimg, .27, .84, chkcoord2, sampcoord2, detector)
+trig2 = bboxtrigger.BboxTrigger(
+    "NE_Moiz", refimg, 0.27, 0.84, chkcoord2, sampcoord2, detector
+)
 
 gallery = galleries.TriggerGallery(getVect)
 gallery.addTrigger(trig1)
@@ -73,7 +82,9 @@ gallery.addTrigger(trig2)
 
 # create trackers for each video/camera
 trackers = {vidnames: Sort() for vidnames in dataloader.getVidNames()}
-outfiles = {vidnames: open(vidnames + 'tmp.txt', "w") for vidnames in dataloader.getVidNames()}
+outfiles = {
+    vidnames: open(vidnames + "tmp.txt", "w") for vidnames in dataloader.getVidNames()
+}
 
 newfilenum = 0
 
@@ -101,7 +112,7 @@ for findex, frames in tqdm(dataloader):
         widths = trkbboxes[:, 2] - trkbboxes[:, 0]
         heights = trkbboxes[:, 3] - trkbboxes[:, 1]
         aspectratio = heights / widths
-        readybools = np.isclose(aspectratio, 2, rtol=.25)
+        readybools = np.isclose(aspectratio, 2, rtol=0.25)
         indexes = np.arange(len(tracks))[readybools]
 
         # iterate through returned bounding boxes
@@ -112,13 +123,16 @@ for findex, frames in tqdm(dataloader):
             if ind in indexes:
                 cropimg = crop_image(frame, box)
                 if cropimg.size > 5:
-                    newname = 'tmpfiles/%07d.jpg' % newfilenum
+                    newname = "tmpfiles/%07d.jpg" % newfilenum
                     newfilenum = newfilenum + 1
                     cv2.imwrite(newname, cropimg)
                     trksoff[ind].save_img(newname)
 
             # write bounding box, frame number, and trackid to file
-            outfiles[vidname].write('%d,%d,%.2f,%.2f,%.2f,%.2f\n' % (findex, trk[4], box[0][0], box[0][1], box[1][0], box[1][1]))
+            outfiles[vidname].write(
+                "%d,%d,%.2f,%.2f,%.2f,%.2f\n"
+                % (findex, trk[4], box[0][0], box[0][1], box[1][0], box[1][1])
+            )
 
         # iterate through new tracks and add their current bounding box to list of track references
         for trk in newtrks:
@@ -126,28 +140,28 @@ for findex, frames in tqdm(dataloader):
             box = ((int(d[0]), int(d[1])), (int(d[2]), int(d[3])))
             cropimg = crop_image(frame, box)
             if cropimg.size > 5:
-                newname = 'tmpfiles/%07d.jpg' % newfilenum
+                newname = "tmpfiles/%07d.jpg" % newfilenum
                 newfilenum = newfilenum + 1
                 cv2.imwrite(newname, cropimg)
                 trk.save_img(newname)
 
 # save images from gallery captured throughout video
 for i, img in enumerate(gallery.people):
-    cv2.imwrite('tmpgal/%03d.jpg' % i, img)
+    cv2.imwrite("tmpgal/%03d.jpg" % i, img)
 
 # load up the gallery (an artifact of debugging)
-mangall = loadPredefGal('tmpgal/')
+mangall = loadPredefGal("tmpgal/")
 
 # write filename for all reference images for each track (artifact of debugging)
 for key in outfiles:
     outfiles[key].close()
-    outfiles[key] = np.loadtxt(key + 'tmp.txt', delimiter=',')
+    outfiles[key] = np.loadtxt(key + "tmp.txt", delimiter=",")
 
     traks = trackers[key]
     tracks = traks.trackers + traks.rejects
-    file = open(key + 'tmp2.txt', "w")
+    file = open(key + "tmp2.txt", "w")
     for trk in tracks:
-        file.write(str(trk.id) + ' ' + ' '.join(trk.imgfiles) + '\n')
+        file.write(str(trk.id) + " " + " ".join(trk.imgfiles) + "\n")
     file.close()
 
 # iterate through trackers for each camera
@@ -166,7 +180,9 @@ for vidname, sorto in trackers.items():
 
             if uniqvect is not None:
                 # find out what is the most similar gallery image
-                dists = [np.average(np.dot(uniqvect, np.transpose(out2))) for out2 in mangall]
+                dists = [
+                    np.average(np.dot(uniqvect, np.transpose(out2))) for out2 in mangall
+                ]
                 index = getMaxIndex(dists, k=1)
                 trk.reid.append(index)
 
@@ -188,7 +204,4 @@ for vidname, sorto in trackers.items():
     outfiles[vidname][:, 1] = convertnp(outfiles[vidname][:, 1])
 
     # save
-    np.savetxt(vidname + '.txt', outfiles[vidname])
-
-
-
+    np.savetxt(vidname + ".txt", outfiles[vidname])
