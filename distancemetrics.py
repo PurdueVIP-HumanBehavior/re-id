9b9e4cd024991a21981acc789767987978605024
msrasheed <moizr95@gmail.com>
2020-01-30 22:11:13 -0500

    renaming function name/calls

--- distancemetrics.py
+++ distancemetrics.py
@@ -1,11 +1,11 @@
 from constants import defaultkey
 import numpy as np
 
-def unitdotprod(vec1, vec2):
+def unit_dot_product(vec1, vec2):
     return np.dot(vec1, np.transpose(vec2))
 
 options = {
-    defaultkey: unitdotprod,
-    "dotprod": unitdotprod
+    defaultkey: unit_dot_product,
+    "dotprod": unit_dot_product
 }
 
