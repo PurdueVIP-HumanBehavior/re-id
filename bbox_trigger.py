6221490881768cfe1b17192df3b7ebee08e4961e
msrasheed <moizr95@gmail.com>
2020-02-12 11:49:55 -0500

    ran timing test

--- bbox_trigger.py
+++ bbox_trigger.py
@@ -1,6 +1,6 @@
 from skimage.measure import compare_ssim
 import cv2
-import cropper
+from utils import crop_image
 
 
 # TODO: (nhendy) docstring
@@ -13,21 +13,21 @@ class BboxTrigger:
         self._check_coords = check_coords
         self._sample_coords = sample_coords
         self._detector = detector  # ideally this is not here in the future either
-        self._ref_img = cv2.cvtColor(cropper.crop_image(ref_img, check_coords),
+        self._ref_img = cv2.cvtColor(crop_image(ref_img, check_coords),
                                      cv2.COLOR_BGR2GRAY)
 
         self._check = 0
 
     def update(self, frames):
-        img = frames[self.camera_id]
-        chkimg = cv2.cvtColor(cropper.crop_image(img, self.check_coords),
+        img = frames[self._camera_id]
+        chkimg = cv2.cvtColor(crop_image(img, self._check_coords),
                               cv2.COLOR_BGR2GRAY)
-        score, diff = compare_ssim(self.ref_img, chkimg, full=True)
+        score, diff = compare_ssim(self._ref_img, chkimg, full=True)
 
         if self._check == 1:
             if score > self._close_thresh:
-                sampimg = cropper.crop_image(img, self._sample_coords)
-                bboxes, scores = self._detector.getBboxes(sampimg)
+                sampimg = crop_image(img, self._sample_coords)
+                bboxes, scores = self._detector.get_bboxes(sampimg)
                 self._check = 0
                 return True, bboxes, sampimg
         else:
