4ca4b2e606c4d29a567ed9902cc3c98bbc625640
msrasheed <moizr95@gmail.com>
2020-03-11 12:44:26 -0400

    line trigger

--- bbox_trigger.py
+++ bbox_trigger.py
@@ -1,6 +1,8 @@
 from skimage.measure import compare_ssim
 import cv2
 from utils import crop_image
+import numpy as np
+import collections
 
 
 # TODO: (nhendy) docstring
@@ -35,3 +37,52 @@ class BboxTrigger:
                 self._check = 1
 
         return False, None, None
+
+"use is completely different from BboxTrigger"
+class VectorTrigger:
+    def __init__(self, video, vector, inpt, length_thresh, frame_offset):
+        tmpvec = np.random.randn(2)
+        invec = np.array([vector[2] - vector[0], vector[3] - vector[1]])
+        tmpvec -= tmpvec.dot(invec) * invec
+        self.ovector = tmpvec / np.linalg.norm(tmpvec)
+        self.midpt = np.array((vector[0] + vector[2])/2, (vector[1] + vector[3])/2)
+        # 1 is in
+        if np.sign(self.ovector.dot(self.midpt - inpt)) != 1: self.ovector = np.zeros(2) - self.ovector
+        self.length_thresh = length_thresh
+        self.frame_offset = frame_offset
+
+        self.video_oi = video
+        self.flags = collections.defaultdict(float)
+        self.prev_val = collections.defaultdict(float)
+
+    def update(self, peoplebboxes):
+        """
+        :param peoplebboxes: numpy array n x 5 with columns in x1, y1, x2, y2, id order
+        :return: indexes to capture
+        """
+        retval = list()
+        feetpoints = np.array([(peoplebboxes[:,0] + peoplebboxes[:,2])/2, np.max(peoplebboxes[:, 1:3], 1)]).transpose()
+        displace_vects = self.midpt - feetpoints
+        disp_mags = np.linalg.norm(displace_vects, axis=1)
+
+        inout = np.sign(displace_vects.dot(self.ovector))
+
+        for i, (val, bboxes) in enumerate(zip(inout, peoplebboxes)):
+            if displace_vects[i] > self.length_thresh: continue
+            id = bboxes[4]
+
+            if self.flags[id] >= 1: self.flags += 1
+            if self.flags[id] >= self.frame_offset:
+                retval.append(i)
+                self.flags[id] = 0
+
+            # -1 - 1 is entering
+            if self.prev_val[id] - val == -2:
+                self.flags[id] = 1
+
+            self.prev_val[id] = val
+
+        return retval
+
+
+
