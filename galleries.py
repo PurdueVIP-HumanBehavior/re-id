1018aa81f77162a2157b67f2b5932100367fd544
msrasheed <moizr95@gmail.com>
2020-03-11 12:44:26 -0400

    line trigger

--- galleries.py
+++ galleries.py
@@ -26,3 +26,23 @@ class TriggerGallery:
     @property
     def people(self):
         return self._people
+
+class TriggerLineGallery:
+    def __init__(self, attribute_extractor, triggers):
+        self.triggers = triggers
+        self._people = list()
+        self._feats = list()
+        self._attribute_extractor = attribute_extractor
+
+    def update(self, video_name, frame, bboxes):
+        for trig in self.triggers:
+            if trig.video_oi != video_name: continue
+            indsToSave = trig.update(bboxes)
+            for index in indsToSave:
+                cropimg = crop_image(frame, bboxes[index, 1:])
+                vect = self._attribute_extractor(cropimg)
+                self._people.append(cropimg)
+                self._feats.append(vect)
+
+    def people(self):
+        return self._people
