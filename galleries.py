7042d441ec5df6250fcc6061611e56876431094f
Moiz Rasheed <moizr95@gmail.com>
2020-03-28 09:50:40 -0400

    Msrasheed/trigger identifier (#23)
    * frame loading
    
    * line trigger
    
    * line trigger
    
    * testing
    
    * line trigger works on msee_old NE_Moiz left door
    
    * capture all subjects of msee old data

--- galleries.py
+++ galleries.py
@@ -1,6 +1,7 @@
 from constants import defaultkey
 from utils import unitdotprod
 from utils import crop_image
+import numpy as np
 
 
 class TriggerGallery:
@@ -24,5 +25,25 @@ class TriggerGallery:
                     self._feats.append(vect)
 
     @property
+    def people(self):
+        return self._people
+
+class TriggerLineGallery:
+    def __init__(self, attribute_extractor, triggers):
+        self.triggers = triggers
+        self._people = list()
+        self._feats = list()
+        self._attribute_extractor = attribute_extractor
+
+    def update(self, video_name, frame, bboxes):
+        for trig in self.triggers:
+            if trig.video_oi != video_name: continue
+            indsToSave = trig.update(bboxes)
+            for index in indsToSave:
+                cropimg = crop_image(frame, np.reshape(bboxes[index, :-1], (2,2)))
+                vect = self._attribute_extractor(cropimg)
+                self._people.append(cropimg)
+                self._feats.append(vect)
+
     def people(self):
         return self._people
