82f183c93c615bf4ca2cdc3540d130d2f3817300
wang3820 <wt200011>
2020-04-08 13:08:00 -0400

    new coordinate extractor

--- new_msee_data/mapping_2d.py
+++ new_msee_data/mapping_2d.py
@@ -1,112 +1,56 @@
 import numpy as np
 import cv2
 import operator
+from tqdm import tqdm
 
 def coord_extraction (img):
-    '''
-    This function takes in an image with blue reference points. It filters out all pixels with the color blue 
-    and then group those points by x-coordinates. It returns the average x,y value of each group as a set of 
-    reference points
-    '''
-    # Load the image
+    coord = []
+    def click(event, x, y, flags, param):
+
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            coord.append([x,y])
+
     image = cv2.imread(img)
-    width = image.shape[1]
     height= image.shape[0]
-    if width < 200 or height < 200:
-        resize_factor = 10
-        dwidth = int(width*resize_factor)
-        dheight= int(height*resize_factor)
-        dim = (dwidth,dheight)
-        resized = cv2.resize(image,dim)
-        hsv = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
-    else:
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Filter blue color
-    lower_blue = np.array([35, 140, 60])
-    upper_blue = np.array([255, 255, 180])
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)  
-    coord=cv2.findNonZero(mask)
-
-    if width < 200 or height < 200:
-        res = cv2.bitwise_and(resized,resized, mask= mask)
-    else:
-        res = cv2.bitwise_and(image,image, mask= mask)
-
-    x_coordinate = np.array([])
-    y_coordinate = np.array([])
-
-    # Put all coordinates that are blue in a list
-    for pt in coord:
-        x,y = np.split(pt[0],2)
-        x_coordinate = np.append(x_coordinate,x[0])
-        y_coordinate = np.append(y_coordinate,y[0])
-
-    coord_list = []
-    for i in range (0,x_coordinate.size):
-        coord_list.append((x_coordinate[i],y_coordinate[i]))
-
-    coord_list.sort(key = operator.itemgetter(0))
-
-    # Group the coordinates
-    x_coordinate = []
-    y_coordinate = []
-    for pt in coord_list:
-        x,y = pt
-        x_coordinate.append(x)
-        y_coordinate.append(y)
-
-    x_coord = [[]]
-    x_coord[0].append(x_coordinate[0])
-    axis_value = 0
-
-    for i in range(1,len(x_coordinate)):
-        if np.abs(x_coordinate[i] - x_coordinate[i-1]) >= 100:
-            x_coord.append([])
-            axis_value += 1
-        x_coord[axis_value].append(x_coordinate[i])
-
-    y_coord = [[]]
-    y_coord[0].append(y_coordinate[0])
-    axis_value = 0
-    incr = 1
-
-    for j in range(1,len(y_coordinate)):
-        if incr == len(x_coord[axis_value]):
-            y_coord.append([])
-            axis_value += 1
-            incr = 0
-        y_coord[axis_value].append(y_coordinate[j])
-        incr += 1
-
-    pt_x = []
-    pt_y = []
-
-    # Calculate average of x/y values of each group
-    for x in x_coord:
-        if width < 200 or height < 200:
-            mean = np.mean(x)
-            mean = int(mean/10)
-        else:
-            mean = int(np.mean(x))
-        pt_x.append(mean)
-
-    for y in y_coord:
-        if width < 200 or height < 200:
-            mean = np.mean(y)
-            mean = int(mean/10)
-        else:
-            mean = int(np.mean(y))
-        pt_y.append(mean)
+    width = image.shape[1]
+    resize_factor = 1920/width if 1920/width < 1080/height else 1080/height
+    dwidth = int(width*resize_factor)
+    dheight= int(height*resize_factor)
+    dim = (dwidth,dheight)
+    resized = cv2.resize(image,dim)
 
 
-    pts = np.stack((pt_x, pt_y), axis=-1)
+    clone = resized.copy()
+    winname = 'Press ESC to exit; Press R to reload; Double Click to Select Reference Points'
+    cv2.namedWindow(winname)
+    cv2.setMouseCallback(winname,click)
+
+    while True:
+        cv2.imshow(winname,resized)
+        #cv2.imshow(winname,image)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord('r'):
+            image = clone.copy()
+            coord= []
+        elif key == 27:
+            break
+
+    cv2.destroyAllWindows()
+
+    coord = np.asarray(coord)
+    coord = coord / resize_factor
+    pts = []
+    for i in coord:
+        pts.append(np.around(i))
+    pts = np.asarray(pts)
+
     return pts
 
 def transform_2d(frame_data):
 	#Camera and floor plan hard coded points
 	camera_pts = coord_extraction('camera_reference.jpg')
-	fl_plan_pts = coord_extraction('2d_reference.jpg')
+	fl_plan_pts = coord_extraction('floor_plan.jpg')
 
 	#Open camera and floor plan
 	cam_img = cv2.imread('00000.jpg',cv2.IMREAD_COLOR)
@@ -120,7 +64,7 @@ def transform_2d(frame_data):
 	c = 1
 	img_array = []
 
-	for frame in frame_data:
+	for frame in tqdm(frame_data):
 		for data in frame:
 			#pt is the point to 2D map
 			pt = np.array([[data[0],data[1]]], dtype='float32')
@@ -146,7 +90,7 @@ def transform_2d(frame_data):
 		fl_plan_img = cv2.imread('floor_plan.jpg',cv2.IMREAD_COLOR)
 		c=c+1
 	video = cv2.VideoWriter('2d_proj_vid.avi',cv2.VideoWriter_fourcc(*'DIVX'),15,size)
-	for i in img_array:
+	for i in tqdm(img_array):
 		video.write(i)
 	video.release()
 
